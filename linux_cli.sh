c6f2d018fdc11b8ed4fe59514073f0541a560901
Marek Nachtman <marek@24s.cz>
2025-06-08 23:33:39 +0200

    Rozšíření linux_cli.sh o práci se soubory, linky a právy

--- linux_cli.sh
+++ linux_cli.sh
@@ -4,3 +4,24 @@ echo "Uživatel: $(whoami)"
 echo "Verze Linuxu:"
 cat /etc/os-release
 
+# Vytvoření zanořených adresářů
+mkdir -p /usr/adresar/podadresar/posledniadresar
+
+# Vytvoření souboru s textem
+echo "Ahoj ze sveta Linux" > /usr/adresar/podadresar/posledniadresar/soubor.txt
+
+# Vytvoření symbolického linku
+ln -sf /usr/adresar/podadresar/posledniadresar/soubor.txt /tmp/softLink
+
+# Zkopírování souboru do /tmp
+cp /usr/adresar/podadresar/posledniadresar/soubor.txt /tmp/
+
+# Výpis UID a GID
+echo "UID: $(id -u)"
+echo "GID: $(id -g)"
+
+# Změna práv: ostatní pouze čtení
+chmod o=r /usr/adresar/podadresar/posledniadresar/soubor.txt
+
+# Změna práv na symbolický link: execute pro vlastníka, žádná práva pro ostatní
+chmod 100 /tmp/softLink
